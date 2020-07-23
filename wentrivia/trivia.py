028f36598cb2c0528f468f13baab2f71c576b840
Manuel Ignacio Pérez Alcolea <perezalcoleam@gmail.com>
2020-07-22 21:28:41 -0300

    Change calls from `str.lower()` to `str.casefold()`

--- wentrivia/trivia.py
+++ wentrivia/trivia.py
@@ -70,12 +70,12 @@ class Trivia:
         """Checks if the given `answer` matches any correct answer to
         the `question`"""
         if isinstance(message, Message):
-            answer = message.content.lower()
+            answer = message.content.casefold()
         else:
-            answer = message.lower()
+            answer = message.casefold()
 
         # TODO: This should be somewhere else. It's being generated each time.
-        correct_answers = (correct.lower() for correct in question.answers)
+        correct_answers = (correct.casefold() for correct in question.answers)
 
         if question.perfect:
             return answer in correct_answers
